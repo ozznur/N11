1e468ea9f0340f336efd8aaca19c2a70e6bef59c
gulserengenc <gencgulseren1@gmail.com>
2020-03-29 17:01:20 +0300

    Favorilere ekleme testi yapıldı.

--- src/test/java/test/LoginPageImplementation.java
+++ src/test/java/test/LoginPageImplementation.java
@@ -1,11 +1,10 @@
-package test;
+package base;
 
 import base.BaseTest;
 import com.thoughtworks.gauge.Step;
 import page.LoginPage;
 
-import static constants.Constant.LOG_IN;
-import static constants.Constant.LOG_IN_BUTTON;
+import static constants.Constant.*;
 
 public class LoginPageImplementation extends BaseTest {
 
@@ -31,10 +30,33 @@ public class LoginPageImplementation extends BaseTest {
         loginPage.sendPassword(password);
     }
 
-
     @Step("Giriş yapa tıkla")
     public void loginClick() {
         loginPage.click(LOG_IN_BUTTON);
     }
 
+    @Step("Ürün <urun> girin")
+    public void sendProduct(String urun) {
+        loginPage.sendKeys(SEARCH_AREA, "Buzdolabı");
+    }
+
+    @Step("Ürün ara butonuna tıkla")
+    public void searchProductButton() {
+        loginPage.click(SEARCH_BUTTON);
+    }
+
+    @Step("Bir <Buzdolabı> seçin")
+    public void selectProduct(Object arg0) {
+        loginPage.click(SELECT_PRODUCT);
+    }
+
+    @Step("Favorilere ekle")
+    public void addFavorites() {
+        loginPage.click(WISH_BUTTON);
+    }
+
+    @Step("Favorilere ekle butonuna tıkla")
+    public void addFavoritesButton() {
+        loginPage.click(ADD_FAVORITES);
+    }
 }
